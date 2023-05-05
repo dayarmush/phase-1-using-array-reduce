7d599290d9894b0b6dbdf85465189b7df77902ec
dovid yarmush <yarmushda@gmail.com>
2023-05-05 17:25:37 -0400

    completed reduce lab

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+let totalBatteries = 0;
+batteryBatches.reduce((accum, batteries) => {
+    totalBatteries = accum + batteries;
+    return totalBatteries;
+}, 0)
